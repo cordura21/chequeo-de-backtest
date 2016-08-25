f57f679a44e1b00f3a13838fb263841b03c929f0
cordura21 <cordura21@gmail.com>
2016-08-25 18:05:21 -0300

    Added backtest curves

--- main.R
+++ main.R
@@ -1,1 +1,20 @@
 # Levantar los índices de Grasso
+library(readxl)
+library(dplyr)
+library(tidyr)
+bt <- list()
+
+bt$raw.data <- readxl::read_excel('indices-4.xlsx') %>%
+  tbl_df() %>% mutate(Date = as.Date(Date)) %>%
+  gather(strat,price,-Date) %>% filter(price != 100)
+
+bt$start.days <- bt$raw.data %>%
+  group_by(strat) %>% summarise(start = first(Date))
+
+bt$returns <- bt$raw.data %>%
+  group_by(strat) %>% mutate(return = TTR::ROC(price,type = 'discrete')) %>%
+  mutate(price = NULL) %>% spread(strat,return) %>% filter(Date >= '2007-01-05')
+
+bt$xts <- xts(bt$returns[,-1], bt$returns$Date)
+
+charts.PerformanceSummary(bt$xts, ylog = TRUE)
